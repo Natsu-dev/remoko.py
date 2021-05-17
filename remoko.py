37c67ca28e5025d2f50c7554e0401b8e5328e27c
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2021-05-17 18:04:13 +0900

    [upd] command process

--- remoko.py
+++ remoko.py
@@ -2,6 +2,9 @@
 import discord
 import platform
 
+import os
+import subprocess
+
 # Custom Libraries
 import config
 import phrases
@@ -33,6 +36,16 @@ async def on_message(message):
     if message.content.startswith('remoko'):
         l = message.content.split()[1:]
 
+        if l[1] == 'pi':
+            if l[2] == 'temp':
+                temp = subprocess.getoutput('vcgencmd measure_temp').split('=')[1]
+                await message.channel.send(temp)
+                print('sent pi temp.')
+            else:
+                await message.channel.send(phrases.invalidArg)
+                print('command args error.')
+                    
+            
         await message.channel.send(l)
         print('command split test')
         return
