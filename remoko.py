50ffd1709013a79da358b8d838109489efdb2e7d
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2021-05-19 22:30:36 +0900

    [add] piStatus func, 'pi' command

--- remoko.py
+++ remoko.py
@@ -13,13 +13,52 @@ import phrases
 client = discord.Client(activity=discord.Game(
     name=platform.system() + ' ' + platform.release()))
 
+# ラズパイのハードウェア情報を出すコマンド
+
+
+def piStatus(l):
+    if l[1] == 'temp':
+        print('Command > pi temp')
+        status = subprocess.getoutput('vcgencmd measure_temp').split('=')[1]
+
+    elif l[1] == 'clock':
+        print('Command > pi clock')
+        status = subprocess.getoutput(
+            'vcgencmd measure_clock arm').split('=')[1]
+
+    elif l[1] == 'volt':
+        print('Command > pi volt')
+        status = subprocess.getoutput(
+            'vcgencmd measure_volts core').split('=')[1]
+
+    elif l[1] == 'mem':
+        print('Command > pi mem')
+        status = subprocess.getoutput('vcgencmd measure_mem arm').split('=')[1]
+
+    else:  # show all
+        print('Command > pi')
+
+        if bool(l[2:]):
+            print('Unknown args after "pi".')
+
+        status = subprocess.getoutput('vcgencmd measure_temp').split('=')[1]
+        status += '\n' + \
+            subprocess.getoutput('vcgencmd measure_clock arm').split('=')[1]
+        status += '\n' + \
+            subprocess.getoutput('vcgencmd measure_volts core').split('=')[1]
+        status += '\n' + \
+            subprocess.getoutput('vcgencmd measure_mem arm').split('=')[1]
+
+    return status
+
 
 @client.event
 async def on_ready():
     print('ready...')
 
 # def commandSwitch(l):
-#     if l[0] == 
+#     if l[0] ==
+
 
 @client.event
 async def on_message(message):
@@ -36,18 +75,12 @@ async def on_message(message):
     if message.content.startswith('remoko'):
         l = message.content.split()[1:]
 
-        if l[1] == 'pi':
-            if l[2] == 'temp':
-                temp = subprocess.getoutput('vcgencmd measure_temp').split('=')[1]
-                await message.channel.send(temp)
-                print('sent pi temp.')
-            else:
-                await message.channel.send(phrases.invalidArg)
-                print('command args error.')
-                    
-            
-        await message.channel.send(l)
-        print('command split test')
+        if l[0] == 'pi':
+            status = piStatus(l)
+            await message.channel.send(status)
+        else:
+            await message.channel.send(l)
+            print('command split test')
         return
 
 client.run(config.TOKEN)
