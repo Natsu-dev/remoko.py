02b963cb9d29a4aebc57eca4597dcf2df80bc4b3
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2021-05-19 23:56:50 +0900

    [upd] all first persons, statusTemp

--- phrases.py
+++ phrases.py
@@ -6,8 +6,8 @@ invalidArg = 'ねー、コマンドの引数間違ってない？'
 invalidCom = 'ねー、コマンド間違ってない？'
 
 # pi commands
-statusTemp = 'あーしの体温は{}だよー'
-statusClock = 'あーしのCPU速度は{}だよー'
-statusVolt = 'あーしのCPu電圧は{}だよー'
-statusMem = 'あーしの使用中メモリは{}だよー'
-statusAll = 'あーしの今の状態はこんな感じだよー\n温度: {}\nCPU速度: {}\nCPU電圧: {}\n使用中メモリ: {}'
+statusTemp = 'あたしの温度は{}だよー'
+statusClock = 'あたしのCPU速度は{}だよー'
+statusVolt = 'あたしのCPu電圧は{}だよー'
+statusMem = 'あたしの使用中メモリは{}だよー'
+statusAll = 'あたしの今の状態はこんな感じだよー\n温度: {}\nCPU速度: {}\nCPU電圧: {}\n使用中メモリ: {}'
