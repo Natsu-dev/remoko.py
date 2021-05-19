8b26d95ec4cbc50a869e6055f134a11d8775c640
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2021-05-19 23:49:05 +0900

    [add] phrases for pi commands

--- phrases.py
+++ phrases.py
@@ -2,6 +2,12 @@
 goodMorning = 'おはよー！'
 
 # commands
-
 invalidArg = 'ねー、コマンドの引数間違ってない？'
 invalidCom = 'ねー、コマンド間違ってない？'
+
+# pi commands
+statusTemp = 'あーしの体温は{}だよー'
+statusClock = 'あーしのCPU速度は{}だよー'
+statusVolt = 'あーしのCPu電圧は{}だよー'
+statusMem = 'あーしの使用中メモリは{}だよー'
+statusAll = 'あーしの今の状態はこんな感じだよー\n温度: {}\nCPU速度: {}\nCPU電圧: {}\n使用中メモリ: {}'
