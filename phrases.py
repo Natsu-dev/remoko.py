28abcf36f01d60c94e569e4689193c294f2ccb3e
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2021-05-21 15:13:33 +0900

    [upd] reversiReady

--- phrases.py
+++ phrases.py
@@ -6,8 +6,11 @@ invalidArg = 'ねー、コマンドの引数間違ってない？'
 invalidCom = 'ねー、コマンド間違ってない？'
 
 # pi commands
-statusTemp = 'あたしの今の温度は{}だよー'
-statusClock = 'あたしの今のCPU速度は{}だよー'
-statusVolt = 'あたしの今のCPU電圧は{}だよー'
-statusMem = 'あたしの今の使用中メモリは{}だよー'
-statusAll = 'あたしの今の状態はこんな感じだよー\n温度: {}\nCPU速度: {}\nCPU電圧: {}\n使用中メモリ: {}'
+statusTemp = 'あたしの今の温度は{}だよー'       # temp
+statusClock = 'あたしの今のCPU速度は{}だよー'   # clock
+statusVolt = 'あたしの今のCPU電圧は{}だよー'    # volt
+statusMem = 'あたしの今の使用中メモリは{}だよー' # mem
+statusAll = 'あたしの今の状態はこんな感じだよー\n温度: {}\nCPU速度: {}\nCPU電圧: {}\n使用中メモリ: {}'  # temp, clock, volt, mem
+
+# reversi commands
+reversiReady = 'リバーシ？ やろーやろー！ {}が {} ね？' # player, color
