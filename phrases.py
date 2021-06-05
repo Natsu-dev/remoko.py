55f5677fa5724e13e9cd10fc82105746050572bb
Natsu-dev <Natsu-aoiro@s.okayama-u.ac.jp>
2021-06-05 20:02:04 +0900

    [add] test phrase

--- phrases.py
+++ phrases.py
@@ -1,19 +1,22 @@
+# Test
+testPhrase = 'テストだよ！'
+
 # Good Morning
 goodMorning = 'おはよー！'
 
 # Love
 love = 'あたしもすきだよ'
 
-# commands
+# Commands
 invalidArg = 'ねー、コマンドの引数間違ってない？'
 invalidCom = 'ねー、コマンド間違ってない？'
 
-# pi commands
+# pi Commands
 statusTemp = 'あたしの今の温度は{}だよー'       # temp
 statusClock = 'あたしの今のCPU速度は{}だよー'   # clock
 statusVolt = 'あたしの今のCPU電圧は{}だよー'    # volt
 statusMem = 'あたしの今の使用中メモリは{}だよー' # mem
 statusAll = 'あたしの今の状態はこんな感じだよー\n温度: {}\nCPU速度: {}\nCPU電圧: {}\n使用中メモリ: {}'  # temp, clock, volt, mem
 
-# reversi commands
+# reversi Commands
 reversiReady = 'リバーシ？ やろーやろー！ {}が {} ね？' # player, color
