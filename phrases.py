712b89e9dc3ab864d8c7829a57c3cd93e63aedf0
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2021-06-06 10:46:34 +0900

    [upd] quotation block in command pi

--- phrases.py
+++ phrases.py
@@ -16,7 +16,7 @@ statusTemp = 'あたしの今の温度は{}だよー'       # temp
 statusClock = 'あたしの今のCPU速度は{}だよー'   # clock
 statusVolt = 'あたしの今のCPU電圧は{}だよー'    # volt
 statusMem = 'あたしの今の使用中メモリは{}だよー' # mem
-statusAll = 'あたしの今の状態はこんな感じだよー\n温度: {}\nCPU速度: {}\nCPU電圧: {}\n使用中メモリ: {}'  # temp, clock, volt, mem
+statusAll = 'あたしの今の状態はこんな感じだよー\n> 温度: {}\n> CPU速度: {}\n> CPU電圧: {}\n> 使用中メモリ: {}'  # temp, clock, volt, mem
 
 # reversi Commands
 reversiReady = 'リバーシ？ やろーやろー！ {}が {} ね？' # player, color
