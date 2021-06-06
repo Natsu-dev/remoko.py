409ab4f6f4d264ceab1bf33a24ffee3096efcd9e
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2021-06-06 20:31:15 +0900

    [add] wolSuccess, wolFailure

--- phrases.py
+++ phrases.py
@@ -18,5 +18,9 @@ statusVolt = 'あたしの今のCPU電圧は{}だよー'    # volt
 statusMem = 'あたしの今の使用中メモリは{}だよー' # mem
 statusAll = 'あたしの今の状態はこんな感じだよー\n> 温度: {}\n> CPU速度: {}\n> CPU電圧: {}\n> 使用中メモリ: {}'  # temp, clock, volt, mem
 
+# wol Commands
+wolSuccess = 'マジックパケットを送信したよ！'
+wolFailure = 'マジックパケット送るの失敗しちゃった…ごめんよ……'
+
 # reversi Commands
 reversiReady = 'リバーシ？ やろーやろー！ {}が {} ね？' # player, color
